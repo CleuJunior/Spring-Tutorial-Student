1411c2c600dfc685bd2e51b3cb8ab14d8673a963
Cleonildo Soares Guimaraes Junior <cleonildo.junior@b2wdigital.com>
2021-05-30 15:18:57 -0300

    JPA

--- src/main/java/com/spring/crud/tutorial/stduent/Student.java
+++ src/main/java/com/spring/crud/tutorial/stduent/Student.java
@@ -1,8 +1,23 @@
 package com.spring.crud.tutorial.stduent;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
+@Entity
+@Table
 public class Student {
+
+    @Id
+    @SequenceGenerator(
+            name = "student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "student_sequence"
+
+    )
     private Long id;
     private String name;
     private Integer age;
